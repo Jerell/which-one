cfc68d4d014ce8d5a36c532bff0d0cc5d3cbe967
Jerell James <jerell1998@gmail.com>
2020-11-19 21:10:56 +0000

    readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,6 @@ import React from "react";
 import { TextField, Box } from "@material-ui/core";
 
 export default function Header(props) {
-  let style = {
-    fontWeight: "bold",
-    textTransform: "uppercase",
-  };
   function handleChange(e) {
     let text = e.target.value;
     props.setWord(text);
